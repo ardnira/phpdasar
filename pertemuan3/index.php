ef548e37c0a15e35e17f7dc5b256ed7989f17b28
ardnira <ardnira@gmail.com>
2020-06-28 22:32:05 +0700

    membuat class
    sip keren membuat class

---
+++ pertemuan3/index.php
@@ -0,0 +1,30 @@
+<?php 
+    class Produk{
+        public $judul="judule opo";
+        public $penulis="penulise sopo";
+        public $harga=700;
+
+        public function sayHello(){
+return "heloooo yooo";
+        }
+    }
+
+    //atau penulisan bs di tulis ini
+    class Mobil{
+        public  $judul="judule opo",
+                $penulis="penulise sopo",
+                $harga=700;
+    }
+
+     
+
+    $buku=new Produk();
+    $buku->penulis ="coba saj";
+    var_dump($buku->penulis);
+
+    $komik=new Produk();
+
+    echo  "komik: $buku->harga , $buku->judul" ;
+    echo "<br>";
+    echo $buku->sayHello();
+ ?>
